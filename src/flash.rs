27a4037aa8d876511d70acbd5c8f26409048a8ae
PVVovan <PVVovan@gmail.com>
2023-09-02 12:51:18 +0200

    Unlock flash

--- src/flash.rs
+++ src/flash.rs
@@ -16,4 +16,26 @@ pub fn setlatency() {
             break;
         }
     }
+
+    init();
+}
+
+const BASESECTOR6: *mut u32 = (0x0800_0000 + 128 * 5) as *mut u32;
+const DEADBEEF: u32 = 0xDEAD_BEEF;
+
+fn init() {
+    if unsafe { core::ptr::read_volatile(BASESECTOR6) } != DEADBEEF {
+        unlock();
+    }
+}
+
+fn unlock() {
+    const KEY1: u32 = 0x4567_0123;
+    const KEY2: u32 = 0xCDEF_89AB;
+    const FLASH_BASE: u32 = 0x5200_2000;
+    const FLASH_KEYR: *mut u32 = (FLASH_BASE + 0x004) as *mut u32;
+    unsafe {
+        core::ptr::write_volatile(FLASH_KEYR, KEY1);
+        core::ptr::write_volatile(FLASH_KEYR, KEY2);
+    }
 }
